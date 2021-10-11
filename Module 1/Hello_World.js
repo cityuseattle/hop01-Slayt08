39fe6288e28b9f6983c11c96ea7970d119ad0516
Stephanie Layton <stephanielayton@Stephanies-MacBook-Air.local>
2021-10-11 00:24:59 -0700

    Submission for Module 1

---
+++ Module 1/Hello_World.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+var message;
+message = 'Hello TypeScript';
+console.log(message);
+//message = 30;
+var bool = [true, false];
+console.log(bool[1]);
---
+++ Module 1/Hello_World.ts
@@ -0,0 +1,7 @@
+console.log("Hello World");
+let message: string;
+message = 'Hello TypeScript';
+console.log(message);
+//message = 30;
+const bool = [true, false];
+console.log(bool[1]);
